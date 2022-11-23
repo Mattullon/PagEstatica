e68006137ef1142c861ad5069894c69a6dfdf778
Mattullon <mattullon@gmail.com>
2022-11-22 23:12:51 -0300

    f

--- src/App.js
+++ src/App.js
@@ -65,12 +65,7 @@ function App() {
             process, soft-skills test and a technical assessment.
           </p>{" "}
           <p className="parrafo">
-            Agile Tribe matches start-ups with the best remote developers,
-            however, just in case you didn't realise, our rates are 50% cheaper
-            than the industry average and we don't charge ridiculous mark-ups
-            (only 25%). To top it off, all our developers have successfully
-            completed our technical vetting which includes a 2-stage interview
-            process, soft-skills test and a technical assessment.
+           
           </p>{" "}
           <Link 
           component="button"
