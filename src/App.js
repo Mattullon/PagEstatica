ff002219d41f8d9daf8918c62480f201c4b2a2d1
Mattullon <mattullon@gmail.com>
2022-12-01 16:18:10 -0300

    comienzo de 3 grid

--- src/App.js
+++ src/App.js
@@ -1,77 +1,37 @@
 import "./App.css";
-import React, { useState} from 'react';
-import { Layout } from "./components/Layout";
-import Footer from "./components/footer";
-import {Button} from "@mui/material"
+import React from "react";
+import { Grid } from "@mui/material";
 function App() {
-  const dark = "App-theme";
-  const light = "App";
-  const [mode,setMode]=useState(true);
-  
-  
-  
-  return (
-    <div className={mode ? light : dark }>
-      <Layout/>
-      
-             <Button onClick={()=>setMode(!mode)}> Darkmode </Button>
-     <div  className="inicio">
-        <h3 className="big"> BIENVENIDOS ! </h3>
-        <h4 className="big">
-          Este seria el Primer Torneo Para Desarrolladores de Apps En Todo el
-          Paraguay !
-        </h4>
-        <div className="Tparrafo">
-          <p className="parrafo">
-            Agile Tribe matches start-ups with the best remote developers,
-            however, just in case you didn't realise, our rates are 50% cheaper
-            than the industry average and we don't charge ridiculous mark-ups
-            (only 25%). To top it off, all our developers have successfully
-            completed our technical vetting which includes a 2-stage interview
-            process, soft-skills test and a technical assessment.
-          </p>{" "}
-          <p className="parrafo">
-          Agile Tribe matches start-ups with the best remote developers,
-            however, just in case you didn't realise, our rates are 50% cheaper
-            than the industry average and we don't charge ridiculous mark-ups
-            (only 25%). To top it off, all our developers have successfully
-            completed our technical vetting which includes a 2-stage interview
-            process, soft-skills test and a technical assessment.
-          </p>
-          <p className="parrafo">
-          Agile Tribe matches start-ups with the best remote developers,
-            however, just in case you didn't realise, our rates are 50% cheaper
-            than the industry average and we don't charge ridiculous mark-ups
-            (only 25%). To top it off, all our developers have successfully
-            completed our technical vetting which includes a 2-stage interview
-            process, soft-skills test and a technical assessment.
-          </p>
-          <p className="parrafo">
-          Agile Tribe matches start-ups with the best remote developers,
-            however, just in case you didn't realise, our rates are 50% cheaper
-            than the industry average and we don't charge ridiculous mark-ups
-            (only 25%). To top it off, all our developers have successfully
-            completed our technical vetting which includes a 2-stage interview
-            process, soft-skills test and a technical assessment.
-          </p>
-          
-        </div>
-        
-          
-       
-      </div> 
-     
+ 
 
-      <Footer></Footer>
-    </div>
-   
+  return (
+<Grid container className="tabla" 
+flexDirection={"column"} 
+justifyItems={"center"}
+
+
+>
+  
+  <Grid item className="caja1">
+    instagram
+  </Grid>
+  
+  <Grid item className="caja2">
+    facebbok
+  </Grid>
+  
+  <Grid item className="caja3">
+    telegram
+  </Grid>
+
+</Grid>
   );
 }
 
 export default App;
 
-//  
+//
 //layout  que este anidado como hijop un componente para quie siempre tenga el appbar
-// drawer que salga las opciones 
-// pie de pag al layout foother 
-// agregarle mas pag 
+// drawer que salga las opciones
+// pie de pag al layout foother
+// agregarle mas pag
